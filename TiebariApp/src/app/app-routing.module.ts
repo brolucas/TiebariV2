cd62ce864e2b3e29c0c7a2e867b201973e819e56
Lucas Brossier <lucasbrossier95@gmail.com>
2019-12-18 19:02:36 +0100

    debug

--- TiebariApp/src/app/app-routing.module.ts
+++ TiebariApp/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   {
     path: 'accueil',
     loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
